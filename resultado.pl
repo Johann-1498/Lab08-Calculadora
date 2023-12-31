7db9e87c40cc04371daeb63a84be168ef7626e5e
Johann-1498 <jcaceresruiz@unsa.edu.pe>
2023-12-30 21:35:02 -0500

    Creacion del html en perl para plasmar la respuesta

--- resultado.pl
+++ resultado.pl
@@ -19,3 +19,7 @@ if($operacion){
     $resultado = $partes[0] / $partes[1];
     }
 }
+print "Content-Type: text/html\n\n";
+print "<html><head><title>RESULTADO</title></head><body>";
+print "<h1>Resultado: $resultado</h1>";
+print "</body></html>";
